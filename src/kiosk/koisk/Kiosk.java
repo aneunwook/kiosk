0baf6697e50bfd22dc2b041d07f0b324c8e505ed
aneunwook <dmsdnr0720@naver.com>
2025-04-24 14:03:01 +0900

    리스트를 클래스 생성자를 통해 값을 할당 & start메서드 구현

---
+++ src/kiosk/koisk/Kiosk.java
@@ -0,0 +1,44 @@
+package kiosk.koisk;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Kiosk {
+    List<MenuItem> menuItemList;
+
+    public Kiosk(List<MenuItem> menuItemList){
+       this.menuItemList = menuItemList;
+    }
+
+    public void start(){
+        Scanner scanner = new Scanner(System.in);
+        int menuNumber;
+
+        while (true) {
+            for (int i = 0; i < menuItemList.size(); i++) {
+                MenuItem menuItem = menuItemList.get(i);
+                System.out.println(i + 1 + ". " + menuItem.name + " || " + menuItem.price + " || " + menuItem.explanation);
+            }
+            System.out.println("0. 종료");
+
+            try {
+                System.out.println();
+
+                System.out.print("주문 번호: ");
+                menuNumber = scanner.nextInt();
+
+                System.out.println("**" + menuItemList.get(menuNumber - 1).name + " || " + menuItemList.get(menuNumber - 1).price + " || " + menuItemList.get(menuNumber - 1).explanation + "**\n");
+
+
+            } catch (InputMismatchException e) {
+                System.out.println("숫자로 입력해 주세요! ");
+                scanner.next();
+            }catch (IndexOutOfBoundsException e){
+                System.out.println("1 ~ 4 까지만 선택해주세요! \n");
+                scanner.nextLine();
+            }
+        }
+    }
+}

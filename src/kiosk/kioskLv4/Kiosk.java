a64250509812b80cbc8baaaad996429cbdc1b463
aneunwook <dmsdnr0720@naver.com>
2025-04-24 21:10:55 +0900

    예외처리 추가

--- src/kiosk/kioskLv4/Kiosk.java
+++ src/kiosk/kioskLv4/Kiosk.java
@@ -16,7 +16,7 @@ public class Kiosk {
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.println("[MAIN MENU]");
+            System.out.println("[ MAIN MENU ]");
 
             for (int i = 0; i < menus.size(); i++) {
                 System.out.println(i + 1 + " " + menus.get(i).category);
@@ -26,43 +26,59 @@ public class Kiosk {
 
             System.out.print("주문 번호: ");
             int menuListNumber = scanner.nextInt();
-
-
-            while (true) {
-                try {
-                    System.out.println();
-
-                    if (menuListNumber >= 1 && menuListNumber <= menus.size()) {
-                        for (int i = 0; i < menus.get(menuListNumber - 1).menuItemList.size(); i++) {
-                            MenuItem menuItem = menus.get(menuListNumber - 1).menuItemList.get(i);
-                            System.out.println(i + 1 + ". " + menuItem.name + " || " + menuItem.price + " || " + menuItem.explanation);
-                        }
-                        System.out.println("0. 뒤로가기");
-                        break;
-                    }
-                } catch (InputMismatchException e) {
-                    System.out.println("숫자로 입력해 주세요! ");
-                    scanner.next();
-                } catch (IndexOutOfBoundsException e) {
-                    System.out.println("1 ~ 4 까지만 선택해주세요! \n");
-                    scanner.nextLine();
-                }
+            if(menuListNumber == 0){
+                System.out.println("종료 되었습니다.");
+                return;
             }
+
+
+            try {
+                System.out.println();
+                System.out.println("[ "+ menus.get(menuListNumber - 1).category.toUpperCase()+ " MENU ]");
+
+                if (menuListNumber >= 1 && menuListNumber <= menus.size()) {
+                    for (int i = 0; i < menus.get(menuListNumber - 1).menuItemList.size(); i++) {
+                        MenuItem menuItem = menus.get(menuListNumber - 1).menuItemList.get(i);
+                        System.out.println(i + 1 + ". " + menuItem.name + " || " + menuItem.price + " || " + menuItem.explanation);
+                    }
+                    System.out.println("0. 뒤로가기");
+
+
+                } else if (menuListNumber == 0) {
+                    scanner.nextLine();
+                    continue;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("숫자로 입력해 주세요! ");
+                scanner.nextLine();
+                continue;
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("1 ~ " + menus.size() + " 까지만 선택해주세요! \n");
+                scanner.nextLine();
+                continue;
+            }
+
 
             while (true){
-                System.out.print("음식을 선택해 주세요");
+                try {
+                    System.out.print("음식을 선택해 주세요: ");
+                    int menuNumber = scanner.nextInt();
+                    System.out.println();
 
-                int menuNumber = scanner.nextInt();
-
-                if(menuNumber >= 1 && menuNumber <= menus.get(menuListNumber - 1).menuItemList.size()){
-                    MenuItem menuItem = menus.get(menuListNumber - 1).menuItemList.get(menuNumber - 1);
-                    System.out.println("**" + menuItem.name + " || " + menuItem.price + " || " + menuItem.explanation + "**\n");
-
+                    if(menuNumber >= 1 && menuNumber <= menus.get(menuListNumber - 1).menuItemList.size()){
+                        MenuItem menuItem = menus.get(menuListNumber - 1).menuItemList.get(menuNumber - 1);
+                        System.out.println("**" + menuItem.name + " || " + menuItem.price + " || " + menuItem.explanation + "**\n");
+                    }else {
+                        System.out.println("해당 음식은 없습니다!!\n");
+                        continue;
+                    }
+                    break;
+                }catch (InputMismatchException e) {
+                    System.out.println("숫자로 입력해 주세요!!!!!!!!!!!!!!!");
+                    scanner.nextLine();
                 }
-                break;
+
             }
-
-
 
 //                if (menuNumber == 0) {
 //                    System.out.println("종료되었습니다!");

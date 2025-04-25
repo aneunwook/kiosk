0afce415e4d3fee5f3d70875d1eaf52c2422c274
aneunwook <dmsdnr0720@naver.com>
2025-04-25 17:19:49 +0900

    도전LV.1 구현

---
+++ src/kiosk/kioskLv4/Cart.java
@@ -0,0 +1,30 @@
+package kiosk.kioskLv4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+    private List<MenuItem> cartItemList = new ArrayList<>();
+    private int total;
+    private double amount;
+
+    public List<MenuItem> getCartItemList() {
+        return cartItemList;
+
+    }
+
+    public void addCartItem(MenuItem menuItem) {
+        cartItemList.add(menuItem);
+        total++;
+        amount += menuItem.getPrice();
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+}
